0bc479974dfc5a7d8d2d6e752103727b9640fc10
Mineralka <Chdv962007@gmail.com>
2023-05-06 18:14:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="15">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <?php
+    //// Task№1
+
+    // const isAdmin = false;
+    // if (isAdmin == true){
+    //     echo "
+    //         <div>Приветствую, Админ!</div><br>
+    //         <button>Войти в панель управления</button>
+    //     ";
+    // }
+    // else{
+    //     echo "
+    //         <span>У вас нет прав доступа.</span>
+    //     ";
+    // }
+
+
+    //// Task№2
+
+    // $lang = 'ru';
+    // switch($lang) 
+    // {
+    //     case "en" : echo "I love pumpkins"; break;
+    //     case "ru" : echo "Я люблю тыквы"; break;
+    //     case "de" : echo "Ich liebe Kürbisse" ; break; 
+    // }
+    // ?>
+</body>
+</html>
