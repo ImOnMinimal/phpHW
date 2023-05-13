1c0789da242655b86f84b5fd5e82b5ccc955f1f0
Mineralka <Chdv962007@gmail.com>
2023-05-13 18:12:29 +0300

    HW

--- index.php
+++ index.php
@@ -10,31 +10,51 @@
 </head>
 <body>
     <?php
-    //// Task№1
+        // $date = '31.12.2013';
+        // echo strtr($date,'.','-')
 
-    // const isAdmin = false;
-    // if (isAdmin == true){
-    //     echo "
-    //         <div>Приветствую, Админ!</div><br>
-    //         <button>Войти в панель управления</button>
-    //     ";
-    // }
-    // else{
-    //     echo "
-    //         <span>У вас нет прав доступа.</span>
-    //     ";
-    // }
+        // $str = "abccab";
+        // $str = str_replace("a", "1", $str);
+        // $str = str_replace("b", "2", $str);
+        // $str = str_replace("c", "3", $str);
+        // echo $str;
 
+        // $str = "j2d1i42k34325f5312j3fg";
+        // $str = str_replace("1", "", $str);
+        // $str = str_replace("2", "", $str);
+        // $str = str_replace("3", "", $str);
+        // $str = str_replace("4", "", $str);
+        // $str = str_replace("5", "", $str);
+        // $str = str_replace("6", "", $str);
+        // $str = str_replace("7", "", $str);
+        // $str = str_replace("8", "", $str);
+        // $str = str_replace("9", "", $str);
+        // echo $str
 
-    //// Task№2
+        // $str = 'abc abc abc';
+        // echo strpos($str, 'b');
 
-    // $lang = 'ru';
-    // switch($lang) 
-    // {
-    //     case "en" : echo "I love pumpkins"; break;
-    //     case "ru" : echo "Я люблю тыквы"; break;
-    //     case "de" : echo "Ich liebe Kürbisse" ; break; 
-    // }
-    // ?>
+        // $str = 'abc abc abc';
+        // echo strrpos($str, 'b');
+
+        // $array = array ('1','2','3','4','5');
+        // $result;
+        // foreach ($array as $value){
+        //     $sum = sqrt($value) + $result;
+        // }
+        // echo $result;
+
+        // $array = array ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', '<strong>Суббота</strong>', '<strong>Воскресенье</strong>');
+        // foreach ($array as $value){
+        //     echo "$value <br>";
+        // }
+
+        // $array = array ('10','20','30','50','235','3000');
+        // foreach ($array as $value){
+        //     if($value[0]==1||$value[0]==2||$value[0]==5){
+        //         echo $value;
+        //     }
+        // }
+    ?>
 </body>
 </html>
